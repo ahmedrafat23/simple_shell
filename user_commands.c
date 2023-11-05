1c0cab71276da471d59aadb46c4fff093409dac3
ahmedrafat23 <ahmedrafatdawood@gmail.com>
2023-11-05 01:16:18 -0700

    f1_f2_f3

---
+++ user_commands.c
@@ -0,0 +1,100 @@
+#include "shell.h"
+
+/**
+ * exitShell - exits the shell
+ * @data: Structure containing potential arguments. Used to maintain
+ * constant function prototype.
+ * Return: exits with a given exit status
+ * (0) if data.argv[0] != "exit"
+ */
+int exitShell(info_t *data)
+{
+	int exitCheck;
+
+	if (data->arguments[1]) /* If there is an exit argument */
+	{
+		exitCheck = stringToInteger(data->arguments[1]);
+		if (exitCheck == -1)
+		{
+			data->status = 2;
+			printError(data, "Illegal number: ");
+			printString(data->arguments[1]);
+			printCharacter('\n');
+			return (1);
+		}
+		data->errorNumber = stringToInteger(data->arguments[1]);
+		return (-2);
+	}
+	data->errorNumber = -1;
+	return (-2);
+}
+
+/**
+ * changeDirectory - changes the current directory of the process
+ * @data: Structure containing potential arguments. Used to maintain
+ * constant function prototype.
+ * Return: Always 0
+ */
+int changeDirectory(info_t *data)
+{
+	char *s, *dir, buffer[1024];
+	int chdirReturn;
+
+	s = getCurrentWorkingDirectory(buffer, 1024);
+	if (!s)
+		printString("TODO: >>getcwd failure emsg here<<\n");
+	if (!data->arguments[1])
+	{
+		dir = getEnvironmentVariable(data, "HOME=");
+		if (!dir)
+			chdirReturn = /* TODO: what should this be? */
+				changeCurrentDirectory((dir = getEnvironmentVariable(data, "PWD=")) ? dir : "/");
+		else
+			chdirReturn = changeCurrentDirectory(dir);
+	}
+	else if (compareStrings(data->arguments[1], "-") == 0)
+	{
+		if (!getEnvironmentVariable(data, "OLDPWD="))
+		{
+			printString(s);
+			printCharacter('\n');
+			return (1);
+		}
+		printString(getEnvironmentVariable(data, "OLDPWD="));
+		printCharacter('\n');
+		chdirReturn = /* TODO: what should this be? */
+			changeCurrentDirectory((dir = getEnvironmentVariable(data, "OLDPWD=")) ? dir : "/");
+	}
+	else
+		chdirReturn = changeCurrentDirectory(data->arguments[1]);
+	if (chdirReturn == -1)
+	{
+		printError(data, "can't cd to ");
+		printString(data->arguments[1]);
+		printCharacter('\n');
+	}
+	else
+	{
+		setEnvironmentVariable(data, "OLDPWD", getEnvironmentVariable(data, "PWD="));
+		setEnvironmentVariable(data, "PWD", getCurrentWorkingDirectory(buffer, 1024));
+	}
+	return (0);
+}
+
+/**
+ * helpCommand - displays help message
+ * @data: Structure containing potential arguments. Used to maintain
+ * constant function prototype.
+ * Return: Always 0
+ */
+int helpCommand(info_t *data)
+{
+	char **argumentsArray;
+
+	argumentsArray = data->arguments;
+	printString("help call works. Function not yet implemented \n");
+	if (0)
+		printString(*argumentsArray); /* temp att_unused workaround */
+	return (0);
+}
+
---
+++ user_commands1.c
@@ -0,0 +1,116 @@
+#include "shell.h"
+
+/**
+ * displayCommandHistory - displays the history list, one command by line,
+ *                        preceded with line numbers, starting at 0.
+ * @data: Structure containing potential arguments. Used to maintain
+ *        constant function prototype.
+ * Return: Always 0
+ */
+int displayCommandHistory(info_t *data)
+{
+    printList(data->commandHistory);
+    return 0;
+}
+
+/**
+ * removeUserAlias - removes user-defined alias
+ * @data: parameter struct
+ * @aliasString: the alias string
+ *
+ * Return: Always 0 on success, 1 on error
+ */
+int removeUserAlias(info_t *data, char *aliasString)
+{
+    char *equalsSign, character;
+    int result;
+
+    equalsSign = findCharacter(aliasString, '=');
+    if (!equalsSign)
+        return 1;
+    character = *equalsSign;
+    *equalsSign = 0;
+    result = deleteNodeAtIndex(&(data->userAliases),
+        findNodeIndex(data->userAliases, startsWithNode(data->userAliases, aliasString, -1)));
+    *equalsSign = character;
+    return result;
+}
+
+/**
+ * setUserAlias - sets user-defined alias
+ * @data: parameter struct
+ * @aliasString: the alias string
+ *
+ * Return: Always 0 on success, 1 on error
+ */
+int setUserAlias(info_t *data, char *aliasString)
+{
+    char *equalsSign;
+
+    equalsSign = findCharacter(aliasString, '=');
+    if (!equalsSign)
+        return 1;
+    if (!*++equalsSign)
+        return removeUserAlias(data, aliasString);
+
+    removeUserAlias(data, aliasString);
+    return (addNodeEnd(&(data->userAliases), aliasString, 0) == NULL);
+}
+
+/**
+ * printUserAlias - prints a user-defined alias string
+ * @aliasNode: the alias node
+ *
+ * Return: Always 0 on success, 1 on error
+ */
+int printUserAlias(list_t *aliasNode)
+{
+    char *aliasName = NULL, *aliasValue = NULL;
+
+    if (aliasNode)
+    {
+        aliasName = findCharacter(aliasNode->str, '=');
+        for (aliasValue = aliasNode->str; aliasValue <= aliasName; aliasValue++)
+            putcharCharacter(*aliasValue);
+        putcharCharacter('\'');
+        printString(aliasName + 1);
+        printString("'\n");
+        return 0;
+    }
+    return 1;
+}
+
+/**
+ * manageUserAliases - manages user-defined aliases
+ * @data: Structure containing potential arguments. Used to maintain
+ * constant function prototype.
+ * Return: Always 0
+ */
+int manageUserAliases(info_t *data)
+{
+    int i = 0;
+    char *aliasChar = NULL;
+    list_t *aliasNode = NULL;
+
+    if (data->argumentCount == 1)
+    {
+        aliasNode = data->userAliases;
+        while (aliasNode)
+        {
+            printUserAlias(aliasNode);
+            aliasNode = aliasNode->next;
+        }
+        return 0;
+    }
+    for (i = 1; data->arguments[i]; i++)
+    {
+        aliasChar = findCharacter(data->arguments[i], '=');
+        if (aliasChar)
+            setUserAlias(data, data->arguments[i]);
+        else
+            printUserAlias(startsWithNode(data->userAliases, data->arguments[i], '='));
+    }
+
+    return 0;
+}
+
