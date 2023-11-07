cf5bcfaf0e8a81278510de3761e0a10cb1efe94c
ahmedrafat23 <ahmedrafatdawood@gmail.com>
2023-11-06 23:27:46 -0800

    f7

--- string_operations.c
+++ string_operations.c
@@ -1,78 +1,88 @@
 #include "shell.h"
 
 /**
- **copy_string - copies a string
- *@destination: the destination string to be copied to
- *@source: the source string
- *@num_chars: the number of characters to be copied
- *Return: the concatenated string
+ * custom_strncpy - copies a string
+ *
+ * @dest_str: the destination string to be copied to
+ * @src_str: the source string
+ * @n_chars: the number of characters to be copied
+ *
+ * Return: the concatenated string
  */
-char *copy_string(char *destination, char *source, int num_chars)
+char *custom_strncpy(char *dest_str, char *src_str, int n_chars)
 {
-	int d_index = 0, s_index = 0;
-	char *result = destination;
+	int index_dest = 0, index_src = 0;
+	char *result = dest_str;
 
-	while (source[s_index] != '\0' && d_index < num_chars - 1)
+	while (src_str[index_src] != '\0' && index_dest < n_chars - 1)
 	{
-		destination[d_index] = source[s_index];
-		d_index++;
-		s_index++;
+		dest_str[index_dest] = src_str[index_src];
+		index_dest++;
+		index_src++;
 	}
 
-	if (d_index < num_chars)
+	if (index_dest < n_chars)
 	{
-		int j = d_index;
-		while (j < num_chars)
+		int j = index_dest;
+		while (j < n_chars)
 		{
-			destination[j] = '\0';
+			dest_str[j] = '\0';
 			j++;
 		}
 	}
 
-	return result;
+	return (result);
 }
 
 /**
- **concat_strings - concatenates two strings
- *@first_str: the first string
- *@second_str: the second string
- *@max_chars: the maximum number of characters to be used
- *Return: the concatenated string
+ * custom_strncat - concatenates two strings
+ *
+ * @dest_str: the first string
+ * @src_str: the second string
+ * @n_chars: the maximum number of characters to be used
+ *
+ * Return: the concatenated string
  */
-char *concat_strings(char *first_str, char *second_str, int max_chars)
+char *custom_strncat(char *dest_str, char *src_str, int n_chars)
 {
-	int f_index = 0, s_index = 0;
-	char *result = first_str;
+	int index_dest = 0, index_src = 0;
+	char *result = dest_str;
 
-	while (first_str[f_index] != '\0')
-		f_index++;
+	while (dest_str[index_dest] != '\0')
+		index_dest++;
 
-	while (second_str[s_index] != '\0' && s_index < max_chars)
+	while (src_str[index_src] != '\0' && index_src < n_chars)
 	{
-		first_str[f_index] = second_str[s_index];
-		f_index++;
-		s_index++;
+		dest_str[index_dest] = src_str[index_src];
+		index_dest++;
+		index_src++;
 	}
 
-	if (s_index < max_chars)
-		first_str[f_index] = '\0';
+	if (index_src < n_chars)
+	{
+		dest_str[index_dest] = '\0';
+	}
 
-	return result;
+	return (result);
 }
 
 /**
- **find_char - locates a character in a string
- *@str_to_search: the string to be searched
- *@char_to_find: the character to look for
- *Return: (str_to_search) a pointer to the memory area str_to_search
+ * custom_strchr - locates a character in a string
+ *
+ * @str_to_search: the string to be searched
+ * @char_to_find: the character to look for
+ *
+ * Return: (str_to_search) a pointer to the memory area str_to_search
  */
-char *find_char(char *str_to_search, char char_to_find)
+char *custom_strchr(char *str_to_search, char char_to_find)
 {
 	do {
 		if (*str_to_search == char_to_find)
-			return str_to_search;
+		{
+			return (str_to_search);
+		}
 	} while (*str_to_search++ != '\0');
 
-	return NULL;
+	return (NULL);
 }
 
