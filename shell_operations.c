17282459269f98b1028ff970a34e8c381ce25632
SeRaG <aserag0101@gmail.com>
2023-11-14 17:15:43 -0800

    c programing

--- shell_operations.c
+++ shell_operations.c
@@ -33,9 +33,7 @@ int hsh(info_t *info, char **av)
 	write_history(info);
 	free_info(info, 1);
 	if (!interactive(info) && info->status)
-	{
 		exit(info->status);
-	}
 	if (builtin_ret == -2)
 	{
 		if (info->err_num == -1)
@@ -49,9 +47,9 @@ int hsh(info_t *info, char **av)
  * find_builtin - finds a builtin command
  * @info: the parameter & return info struct
  * Return: -1 if builtin not found,
- * 	0 if builtin executed successfully,
- * 	1 if builtin found but not successful,
- * 	2 if builtin signals exit()
+ * 0 if builtin executed successfully,
+ * 1 if builtin found but not successful,
+ * 2 if builtin signals exit()
  */
 
 int find_builtin(info_t *info)
@@ -76,82 +74,4 @@ int find_builtin(info_t *info)
 			break;
 		}
 	return (built_in_ret);
-}
-
-/**
- * fork_cmd - forks an exec thread to run cmd
- * @info: the parameter & return info struct
- * Return: void
- */
-
-void fork_cmd(info_t *info)
-{
-	pid_t child_pid;
-
-	child_pid = fork();
-	if (child_pid == -1)
-	{
-		perror("Error:");
-		return;
-	}
-	if (child_pid == 0)
-	{
-		if (execve(info->path, info->argv, get_environ(info)) == -1)
-		{
-			free_info(info, 1);
-			if (errno == EACCES)
-				exit(126);
-			exit(1);
-		}
-	}
-	else
-	{
-		wait(&(info->status));
-		if (WIFEXITED(info->status))
-		{
-			info->status = WEXITSTATUS(info->status);
-			if (info->status == 126)
-				print_error(info, "Permission denied\n");
-		}
-	}
-}
-
-/**
- * find_cmd - finds a command in PATH
- * @info: the parameter & return info struct
- * Return: void
- */
-
-void find_cmd(info_t *info)
-{
-	char *path = NULL;
-	int j, k;
-
-	info->path = info->argv[0];
-	if (info->linecount_flag == 1)
-	{
-		info->line_count++;
-		info->linecount_flag = 0;
-	}
-	for (j = 0, k = 0; info->arg[j]; j++)
-		if (!is_delim(info->arg[j], " \t\n"))
-			k++;
-	if (!k)
-		return;
-	path = find_path(info, _getenv(info, "PATH="), info->argv[0]);
-	if (path)
-	{
-		info->path = path;
-		fork_cmd(info);
-	}
-	else
-	{
-		if ((interactive(info) || _getenv(info, "PATH=") || info->argv[0][0] == '/') && is_cmd(info, info->argv[0]))
-			fork_cmd(info);
-		else if (*(info->arg) != '\n')
-		{
-			info->status = 127;
-			print_error(info, "not found\n");
-		}
-	}
 }
