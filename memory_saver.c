5b6dee153639f51fe5cb36299a70655c136dd073
ahmedrafat23 <ahmedrafatdawood@gmail.com>
2023-11-16 15:34:10 -0800

    sd

--- memory_saver.c
+++ memory_saver.c
@@ -24,16 +24,16 @@ int _free(void **ptr)
 
 void free_list(list_t **head)
 {
-        list_t *current = *head;
-        list_t *nextNode;
+	list_t *current = *head;
+	list_t *nextNode;
 
-        while (current != NULL)
-        {
-                nextNode = current->next;
-                free(current->index);
-                free(current->content);
-                free(current);
-                current = nextNode;
-        }
-        *head = (NULL);
+	while (current != NULL)
+	{
+	nextNode = current->next;
+	free(current->index);
+	free(current->content);
+	free(current);
+	current = nextNode;
+	}
+	*head = (NULL);
 }
